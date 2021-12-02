3c4f2cf693382134d30d42497d7f930d236ebe94
tohmyy <71967643+tohmyy@users.noreply.github.com>
2021-12-02 19:53:56 +0100

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -58,6 +58,7 @@ class Order(models.Model):
     customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
     #foreign key gets the list of customers created from customer class
     product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    quantity = models.IntegerField(default=1, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
 
